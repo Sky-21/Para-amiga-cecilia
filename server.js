98d9ff9022ec0c4579af227826f47367747ab694
Allan <sky.dev212@gmail.com>
2020-04-25 09:43:22 -0300

    alterando o backend

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 
 const express = require('express')
+const path = require('path')
 
 const app =  express()
 
-const http = require('http').Server(app)
+const http = require('http').createServer(app)
 
 const port = process.env.PORT || 8080
 
 app.use(express.static(__dirname + '/public'))
 
+app.set('views', path.join(__dirname + '/public'))
+
+
+
+app.engine('html', require('ejs').renderFile )
+
+app.set('view engine', 'html')
+
+
+app.use('/', (req, res) =>{
+  res.render('index.html')
+})
 
 http.listen(port , () => console.log(`rodando na porta: ${port}`))
 
